dce34c7eba53c8201b8df54f52bff4c2c3380384
Vishal <vishalpal1501@gmail.com>
2024-09-25 15:21:42 +0530

    Game board "X " done Updating object State

--- Tic_/src/components/Player.jsx
+++ Tic_/src/components/Player.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [inpurValue, setInputValue] = useState();
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+  const handleSave = () => {
+    setIsEditing(false);
+  };
   return (
     <li>
-      <span className="player">
-        <span className="player-name">{name}</span>
-        <span className="player-symbol">{symbol}</span>
-      </span>
-      <button>Edit</button>
+      {!isEditing ? (
+        <span className="player">
+          <span className="player-name">{name}</span>
+          <span className="player-symbol">{symbol}</span>
+        </span>
+      ) : (
+        <input
+          type="text"
+          className="player-name"
+          value={name}
+          onChange={() => setInputValue()}
+        />
+      )}
+      {!isEditing ? (
+        <button onClick={handleEdit}>Edit</button>
+      ) : (
+        <button onClick={handleSave}>Save</button>
+      )}
     </li>
   );
 }
