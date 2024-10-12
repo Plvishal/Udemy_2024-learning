c99f973549271da9b39258e41d4829365a0c5793
Vishal <vishalpal1501@gmail.com>
2024-10-12 12:55:29 +0530

    first part has  been done

--- investment calculator/src/components/Header.jsx
+++ investment calculator/src/components/Header.jsx
@@ -1,1 +1,13 @@
+// import React from 'react';
+import logo from '../assets/investment-calculator-logo.png';
 
+function Header() {
+  return (
+    <section id="header">
+      <img src={logo} alt="logo image error" />
+      <h1>Investment Calculator</h1>
+    </section>
+  );
+}
+
+export default Header;
