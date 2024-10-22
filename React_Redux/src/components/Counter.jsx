4a967040c01fc8effd98b7e0b4fa76fa0743cae6
Vishal <vishalpal1501@gmail.com>
2024-10-22 15:57:22 +0530

    working with multiple state properties

--- React_Redux/src/components/Counter.jsx
+++ React_Redux/src/components/Counter.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 function Counter() {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
   };
@@ -12,19 +13,29 @@ function Counter() {
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
   return (
     <>
       <h1>Redux Counter</h1>
-      <div>{counter}</div>
-      <button style={styles.button} onClick={incrementHandler}>
-        Increment
-      </button>
-      <button style={styles.button} onClick={incrementDynamicHander}>
-        Increment BY 5
-      </button>
-      <button style={styles.button} onClick={decrementHandler}>
-        Decrement
-      </button>
+      {show && <div>{counter}</div>}
+      <div>
+        <button style={styles.button} onClick={incrementHandler}>
+          Increment
+        </button>
+        <button style={styles.button} onClick={incrementDynamicHander}>
+          Increment BY 5
+        </button>
+        <button style={styles.button} onClick={decrementHandler}>
+          Decrement
+        </button>
+      </div>
+      <div>
+        <button onClick={toggleCounterHandler} style={styles.button}>
+          Toggle Counter
+        </button>
+      </div>
     </>
   );
 }
