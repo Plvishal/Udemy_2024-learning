82756b912bc9580655aa59a3606e06755eefd251
Vishal <vishalpal1501@gmail.com>
2024-10-26 22:17:57 +0530

    How to define routes in react

--- React_Food/src/App.jsx
+++ React_Food/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home';
+import Product from './components/Product';
 
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/product', element: <Product /> },
+]);
 function App() {
-  return (
-    <>
-      <div></div>
-    </>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
