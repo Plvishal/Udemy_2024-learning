b07111635ac9d05ca8259dd6cb39c27cb92bf774
Vishal <vishalpal1501@gmail.com>
2024-10-27 22:17:01 +0530

    show error page

--- React_Food/src/App.jsx
+++ React_Food/src/App.jsx
@@ -3,11 +3,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home';
 import Product from './components/Product';
 import Root from './components/Root';
+import Error from './components/Error';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Home /> },
       { path: '/product', element: <Product /> },
---
+++ React_Food/src/components/Error.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+import MainNavigation from './MainNavigation';
+
+function Error() {
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        <h1>An error occurred</h1>
+        <p>Could not find this page!</p>
+      </main>
+    </>
+  );
+}
+
+export default Error;
