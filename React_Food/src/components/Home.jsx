254d7d4e118fb2de96fbc8d7c50897e9bc7ec775
Vishal <vishalpal1501@gmail.com>
2024-10-27 22:29:56 +0530

    Navigate Programitically

--- React_Food/src/components/Home.jsx
+++ React_Food/src/components/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
-  return <div>Home Page</div>;
+  const navigate = useNavigate();
+  function navigateHandler() {
+    navigate('/product');
+  }
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
+    </>
+  );
 }
 
 export default Home;
