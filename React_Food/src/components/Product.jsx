c983cf8ebba018cbd3d949dfb272535b985301ef
Vishal <vishalpal1501@gmail.com>
2024-10-27 22:45:17 +0530

    Dynamic path parameters

--- React_Food/src/components/Product.jsx
+++ React_Food/src/components/Product.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Product() {
+  const PRODUCT = [
+    { id: 'p1', title: 'Product 1' },
+    { id: 'p2', title: 'Product 2' },
+    { id: 'p3', title: 'Product 3' },
+  ];
   return (
     <>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        {PRODUCT.map((product) => (
+          <li key={product.id}>
+            <Link to={`/product/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
