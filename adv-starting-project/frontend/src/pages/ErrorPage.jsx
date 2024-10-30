4b799ab292570519f36aed4c8d2958b3f7167ab1
Vishal <vishalpal1501@gmail.com>
2024-10-30 12:33:18 +0530

    the json utility provide by react router dom

--- adv-starting-project/frontend/src/pages/ErrorPage.jsx
+++ adv-starting-project/frontend/src/pages/ErrorPage.jsx
@@ -1,16 +1,18 @@
 // import React from 'react'
 import { useRouteError } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
 function ErrorPage() {
   const error = useRouteError();
   const title = 'Error an occured';
   let message = 'Something went wrong';
   console.log(error);
   if (error.status === 500) {
-    message = JSON.parse(error.data).message;
+    message = error.data.message;
   }
 
   return (
     <>
+      <MainNavigation />
       <div
         style={{
           display: 'flex',
--- adv-starting-project/frontend/src/pages/EventPage.jsx
+++ adv-starting-project/frontend/src/pages/EventPage.jsx
@@ -1,6 +1,6 @@
 // import { useEffect, useState } from 'react';
 // import { Link } from 'react-router-dom';
-import { useLoaderData } from 'react-router-dom';
+import { json, useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 // const DUMMY_Events = [
@@ -49,13 +49,20 @@ function EventPage() {
 export default EventPage;
 
 export async function loader() {
-  const response = await fetch('http://localhost:8080/evets');
+  const response = await fetch('http://localhost:8080/evens');
   if (!response.ok) {
     // ......
     // return { isError: true, message: 'Could not fetch events' }; // first way error handling
-    throw new Response(JSON.stringify({ message: 'Could not fetch events' }), {
-      status: 500,
-    });
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events' }), {
+    //   status: 500,
+    // });
+
+    throw json(
+      {
+        message: 'Could not fetch events',
+      },
+      { status: 500 }
+    );
   } else {
     return response;
   }
