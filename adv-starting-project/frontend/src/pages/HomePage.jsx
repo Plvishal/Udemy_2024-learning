02756ff02ba5526cf61eecd0079f764bd2132268
Vishal <vishalpal1501@gmail.com>
2024-10-31 01:22:43 +0530

    finish reacr router dome here

--- adv-starting-project/frontend/src/pages/HomePage.jsx
+++ adv-starting-project/frontend/src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+// import React from 'react';
+import PageContent from '../components/PageContent';
 
 function HomePage() {
   return (
     <>
-      <h1>Home Page</h1>
+      <PageContent title={'Welcome !'}>
+        <p>Manage some new events</p>
+      </PageContent>
     </>
   );
 }
