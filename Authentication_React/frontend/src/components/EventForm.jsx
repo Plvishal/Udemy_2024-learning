78c3c12b3e288998a417708a7dfc7eb16027a476
Vishal <vishalpal1501@gmail.com>
2024-11-01 10:45:47 +0530

    Authorization given to the add and edit route also

--- Authentication_React/frontend/src/components/EventForm.jsx
+++ Authentication_React/frontend/src/components/EventForm.jsx
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
+import { getAuthToken } from '../util/auth';
 
 function EventForm({ method, event }) {
   const data = useActionData();
@@ -100,11 +101,12 @@ export async function action({ request, params }) {
     const eventId = params.eventId;
     url = 'http://localhost:8080/events/' + eventId;
   }
-
+  const token = getAuthToken();
   const response = await fetch(url, {
     method: method,
     headers: {
       'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + token,
     },
     body: JSON.stringify(eventData),
   });
@@ -112,7 +114,7 @@ export async function action({ request, params }) {
   if (response.status === 422) {
     return response;
   }
-  console.log(response);
+
   if (!response.ok) {
     throw json({ message: 'Could not save event.' }, { status: 500 });
   }
